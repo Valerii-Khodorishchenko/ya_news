0d70d9f8af54cd27a1dafc1e91406ddc3a633345
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-07-19 01:36:27 +0500

    add test_news_order

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
@@ -11,26 +13,19 @@ class TestHomePage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        # # Если объектов порядка десяти
-        # for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-        #     News.objects.create(title=f'Новость {index}', text='Просто текст.')
-        #     # Или тоже самое
-        #     news = News(title=f'Новость {index}', text='Просто текст.')
-        #     news.save()
-
-        # # Если объектов порядка сотен и больше. А ЛУЧШЕ ВСЕГДА!!!
-        # all_news = []
-        # for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-        #     news = News(title=f'Новость {index}', text ='Просто текст.')
-        #     all_news.append(news)
-        # News.objects.bulk_create(all_news)
-
-        #   ЕЩЕ КРУЧЕ ЮЗАТЬ ЛИСТКОМПРИХЕНШЕН:
+        # Вычисляем текущую дату.
+        today = datetime.today()
         News.objects.bulk_create(
-            News(title=f'Новость{index}', text='Просто текст.')
+            News(
+                title=f'Новость{index}',
+                text='Просто текст.',
+                # Для каждой новости уменьшаем дату на index дней от tody,
+                # где index - счётчик цикла.
+                date=today - timedelta(days=index)
+            )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         )
-    
+
     def test_news_count(self):
         # Загружаем главную страницу.
         response = self.client.get(self.HOME_URL)
@@ -49,3 +44,12 @@ class TestHomePage(TestCase):
         news_count = object_list.count()
         # Проверяем что на странице именно 10 новостей.
         self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['news_list']
+        # Получаем даты новостей в том порядке, как они выведены на странице.
+        all_danes = [news.date for news in object_list]
+        sorted_dates = sorted(all_danes, reverse=True)
+        # Проверяем, что исходный список был отсортирован правильно.
+        self.assertEqual(all_danes, sorted_dates)
