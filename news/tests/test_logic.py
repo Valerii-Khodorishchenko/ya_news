8b9737417032b125bf3c080ff505f38939a4c8be
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-07-20 14:53:00 +0500

    add test_user_cant_use_bad_words

---
+++ news/tests/test_logic.py
@@ -0,0 +1,69 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+# Импортируем из файла с формами список стоп-слов и предупреждение формы.
+from news.forms import BAD_WORDS, WARNING
+from news.models import Comment, News
+
+User = get_user_model()
+
+
+class TestCommentCreation(TestCase):
+    COMMENT_TEXT = 'Текст сомментария'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Адрес страницы с новостью.
+        cls.url = reverse('news:detail', args=(cls.news.id,))
+        # Создаём пользователя и клиент, логинимся в клиенте.
+        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.user)
+        # Даныне для POST-запроса при создании комментария.
+        cls.form_data = {'text': cls.COMMENT_TEXT}
+
+    def test_anonymous_user_cant_create_comment(self):
+        # Совершаем запрос от анонимного клиента, в POST-запросе отправляем
+        # предварительно подготовленные данные формы с текстом комментария.
+        self.client.post(self.url, data=self.form_data)
+        # Считаем количество комментариев.
+        comments_count = Comment.objects.count()
+        # Ожидаем, что комментариев в базе нет - сравниваем с нулём.
+        self.assertEqual(comments_count, 0)
+
+    def test_user_can_create_comment(self):
+        # Совершаем запрос через авторизованный клиент.
+        response = self.auth_client.post(self.url, data=self.form_data)
+        # Проверяем, что редирект привёл к разделу с комментами.
+        self.assertRedirects(response, f'{self.url}#comments')
+        # Считаем количество комментариев.
+        comments_count = Comment.objects.count()
+        # Убеждаемся, что есть один комментарий.
+        self.assertEqual(comments_count, 1)
+        # Получаем объект комментария из базы.
+        comment = Comment.objects.get()
+        #  Проверяем, что все атрибуты комментария совпадают с ожидаемыми.
+        self.assertEqual(comment.text, self.COMMENT_TEXT)
+        self.assertEqual(comment.news, self.news)
+        self.assertEqual(comment.author, self.user)
+
+    def test_user_cant_use_bad_words(self):
+        # Формируем данные для отправки формы; текст включает
+        # первое слово из списка стоп-слов.
+        bad_words_data = {'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'}
+        # Отправляем запрос через авторизованный клиент.
+        response = self.auth_client.post(self.url, data=bad_words_data)
+        # Проверяем, есть ли в ответе ошибка формы.
+        self.assertFormError(
+            response,
+            form='form',
+            field='text',
+            errors=WARNING
+        )
+        # Дополнительно убедимся, что комментарий не был создан.
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 0)
